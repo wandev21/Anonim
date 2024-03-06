c44621db2c91454d3e26fa09a16e8dd7f2cf601e
Dev Finder <101172343+wandev21@users.noreply.github.com>
2024-03-06 22:39:29 +0700

    Update profile.py

--- keyboards/anonchat/profile.py
+++ keyboards/anonchat/profile.py
@@ -4,9 +4,9 @@ from aiogram.types import ReplyKeyboardRemove
 def profile_menu() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
-    builder.button(text='Изменить возраст', callback_data='change_age')
-    builder.button(text='Изменить пол', callback_data='change_gender')
-    builder.button(text='Изменить чат', callback_data='change_chat')
+    builder.button(text='ubah usia', callback_data='change_age')
+    builder.button(text='ubah jenis kelamin', callback_data='change_gender')
+    builder.button(text='sunting obrolan', callback_data='change_chat')
 
     builder.adjust(1)
     return builder.as_markup(resize_keyboard=True)
