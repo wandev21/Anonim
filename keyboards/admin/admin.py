249d4fe95e267e84fa598d2aaed4bb088b05ab8d
Dev Finder <101172343+wandev21@users.noreply.github.com>
2024-03-06 22:50:41 +0700

    Update admin.py

--- keyboards/admin/admin.py
+++ keyboards/admin/admin.py
@@ -2,31 +2,31 @@ from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton
 
 
-CANCEL_BTN = InlineKeyboardButton(text='🚫 Отмена', callback_data="cancel_admin")
+CANCEL_BTN = InlineKeyboardButton(text='🚫 membatalkan', callback_data="cancel_admin")
 
 
 def main_menu() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
     builder.row(
-        InlineKeyboardButton(text='📥 Рассылка', callback_data="notifications"),
-        InlineKeyboardButton(text='✏️ Подписки', callback_data="subs")
+        InlineKeyboardButton(text='📥 masuk', callback_data="notifications"),
+        InlineKeyboardButton(text='✏️ langganan', callback_data="subs")
     )
     builder.row(
-        InlineKeyboardButton(text='👁 Показы', callback_data="views"),
-        InlineKeyboardButton(text='👋 Приветствия', callback_data="greetings")
+        InlineKeyboardButton(text='👁 tayangan', callback_data="views"),
+        InlineKeyboardButton(text='👋 Salam', callback_data="greetings")
     )
     builder.row(
-        InlineKeyboardButton(text='⛓ Ссылки', callback_data="refs"),
-        InlineKeyboardButton(text="👥 Все юзеры", callback_data='CSV')
+        InlineKeyboardButton(text='⛓ tautan', callback_data="refs"),
+        InlineKeyboardButton(text="👥 semua pengguna", callback_data='CSV')
     )
     builder.row(
-        InlineKeyboardButton(text='👥 Живые юзеры', callback_data="csv"),
-        InlineKeyboardButton(text='🔄 Обновить файл валида', callback_data="updateCsv")
+        InlineKeyboardButton(text='👥 pengguna langsung', callback_data="csv"),
+        InlineKeyboardButton(text='🔄 perbarui file yang valid', callback_data="updateCsv")
     )
     builder.row(
-        InlineKeyboardButton(text='📊 Статистика', callback_data="stats"),
-        InlineKeyboardButton(text='♿️ Удалить неактивных', callback_data="deleteInactive")
+        InlineKeyboardButton(text='📊 statistik', callback_data="stats"),
+        InlineKeyboardButton(text='♿️ hapus tidak aktif', callback_data="deleteInactive")
     )
     builder.row(
         CANCEL_BTN
@@ -38,11 +38,11 @@ def main_menu() -> InlineKeyboardMarkup:
 def generate_or_cancel_ref():
     builder = InlineKeyboardBuilder()
     builder.button(
-        text='🌀 Сгенерировать',
+        text='🌀 menghasilkan',
         callback_data='gen_ref'
     )
     builder.button(
-        text='🚫 Отмена',
+        text='🚫 membatalkan',
         callback_data='refs'
     )
     builder.adjust(1)
@@ -52,7 +52,7 @@ def generate_or_cancel_ref():
 def cancel_action(return_to: str):
     builder = InlineKeyboardBuilder()
     builder.button(
-        text='🚫 Отмена',
+        text='🚫 membatalkan',
         callback_data=return_to
     )
     return builder.as_markup()
@@ -61,11 +61,11 @@ def cancel_action(return_to: str):
 def user_stats_menu() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text='🐵 Сводка по пользователям', callback_data='stats'),
+        InlineKeyboardButton(text='🐵 ringkasan pengguna', callback_data='stats'),
         width=1
     )
     builder.row(
-        InlineKeyboardButton(text='🔙 Назад', callback_data="admin"),
+        InlineKeyboardButton(text='🔙 kembali', callback_data="admin"),
         CANCEL_BTN
     )
 
@@ -75,7 +75,7 @@ def user_stats_menu() -> InlineKeyboardMarkup:
 def back_to_menu(with_cancel: bool = True) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text='🔙 Назад', callback_data="admin"),
+        InlineKeyboardButton(text='🔙 kembali', callback_data="admin"),
         width=2
     )
     if with_cancel:
@@ -90,10 +90,10 @@ def mandatory_subs_list(subs_list: list):
     for num, sub in enumerate(subs_list, start=1):
         builder.row(InlineKeyboardButton(text=f'{sub.channel_name}', callback_data="manageSub_{}".format(sub.id)))
 
-    builder.row(InlineKeyboardButton(text='🆕 Добавить новый', callback_data="createSub"))
-    builder.row(InlineKeyboardButton(text='🤖 Добавить нового бота', callback_data="createSubBot"))
+    builder.row(InlineKeyboardButton(text='🆕 tambah baru', callback_data="createSub"))
+    builder.row(InlineKeyboardButton(text='🤖 tambahkan bot baru', callback_data="createSubBot"))
     builder.row(
-        InlineKeyboardButton(text='🔙 Назад', callback_data="admin"),
+        InlineKeyboardButton(text='🔙 kembali', callback_data="admin"),
         CANCEL_BTN
     )
 
@@ -103,9 +103,9 @@ def mandatory_subs_list(subs_list: list):
 def delete_sub(sub_id: int):
     builder = InlineKeyboardBuilder()
 
-    builder.row(InlineKeyboardButton(text='❌ Удалить ОП', callback_data="deleteSub_{}".format(sub_id)))
+    builder.row(InlineKeyboardButton(text='❌ hapus op', callback_data="deleteSub_{}".format(sub_id)))
     builder.row(
-        InlineKeyboardButton(text='🔙 Назад', callback_data="admin"),
+        InlineKeyboardButton(text='🔙 kembali', callback_data="admin"),
         CANCEL_BTN
     )
 
@@ -118,9 +118,9 @@ def current_views_list(views_list: list):
     for view in views_list:
         builder.row(InlineKeyboardButton(text='{}'.format(view.name), callback_data="manageView_{}".format(view.id)))
 
-    builder.row(InlineKeyboardButton(text='🆕 Создать новый', callback_data="CreateView"))
+    builder.row(InlineKeyboardButton(text='🆕 membuat baru', callback_data="CreateView"))
     builder.row(
-        InlineKeyboardButton(text='🔙 Назад', callback_data="admin"),
+        InlineKeyboardButton(text='🔙 kembali', callback_data="admin"),
         CANCEL_BTN
     )
 
@@ -130,10 +130,10 @@ def current_views_list(views_list: list):
 def view_options(view_id: int):
     builder = InlineKeyboardBuilder()
 
-    builder.row(InlineKeyboardButton(text='❌ Удалить показ', callback_data="deleteView_{}".format(view_id)))
-    builder.row(InlineKeyboardButton(text='👁 Посмотреть показ', callback_data="watchView_{}".format(view_id)))
+    builder.row(InlineKeyboardButton(text='❌ hapus pertunjukan', callback_data="deleteView_{}".format(view_id)))
+    builder.row(InlineKeyboardButton(text='👁 lihat pertunjukan', callback_data="watchView_{}".format(view_id)))
     builder.row(
-        InlineKeyboardButton(text='🔙 Назад', callback_data="views"),
+        InlineKeyboardButton(text='🔙 kembali', callback_data="views"),
         CANCEL_BTN
     )
 
@@ -142,7 +142,7 @@ def view_options(view_id: int):
 
 def refresh_notifications_status():
     builder = InlineKeyboardBuilder()
-    builder.row(InlineKeyboardButton(text="Обновить", callback_data='refreshNotify'))
+    builder.row(InlineKeyboardButton(text="memperbarui", callback_data='refreshNotify'))
 
     return builder.as_markup()
 
@@ -150,14 +150,14 @@ def refresh_notifications_status():
 def where_to_send_notifications_menu() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text="👤 ПОЛЬЗОВАТЕЛИ", callback_data="users_notifications"),
-        InlineKeyboardButton(text="👥 ГРУППЫ", callback_data="chats_notifications")
+        InlineKeyboardButton(text="👤 pengguna", callback_data="users_notifications"),
+        InlineKeyboardButton(text="👥 kelompok", callback_data="chats_notifications")
     )
     builder.row(
-        InlineKeyboardButton(text="🌐 Везде", callback_data="all_notifications")
+        InlineKeyboardButton(text="🌐 di mana pun", callback_data="all_notifications")
     )
     builder.row(
-        InlineKeyboardButton(text='🔙Вернуться в меню', callback_data="admin"),
+        InlineKeyboardButton(text='🔙 kembali ke menu', callback_data="admin"),
         CANCEL_BTN
     )
 
@@ -168,11 +168,11 @@ def refs_menu():
     builder = InlineKeyboardBuilder()
 
     builder.row(
-        InlineKeyboardButton(text='🆕 Добавить', callback_data="createref"),
-        InlineKeyboardButton(text="❌ Удалить", callback_data="delrefs")
+        InlineKeyboardButton(text='🆕 menambahkan', callback_data="createref"),
+        InlineKeyboardButton(text="❌ menghapus", callback_data="delrefs")
     )
     builder.row(
-        InlineKeyboardButton(text='🔙 Назад', callback_data="admin"),
+        InlineKeyboardButton(text='🔙 kembali', callback_data="admin"),
         CANCEL_BTN
     )
 
